36fb62aa1f54e70e89a102a07a8200ab205c11cc
zener <129259374+zener9233@users.noreply.github.com>
2023-06-13 20:30:36 +0900

    Add files via upload

---
+++ beak/No.1436.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+        
+        while(count != n)
+        {
+        	num++;
+        	if(String.valueOf(num).contains("666"))
+        		count++;
+        }
+        
+        bw.write(num+"");
+        br.close();
+        bw.flush();
+        bw.close();
+	}
+
+}
